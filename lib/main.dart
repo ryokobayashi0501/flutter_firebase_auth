197250c5d3c81b18e8ec94d4ec53ee7ee3ad0ad5
ryokobayashi0501 <ryokoba0501@gmail.com>
2025-04-24 10:42:32 +0900

    signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,43 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_user_auth/screen/forget_password.dart';
 import 'package:flutter_simple_user_auth/screen/home.dart';
 import 'package:flutter_simple_user_auth/screen/login.dart';
 import 'package:flutter_simple_user_auth/screen/signup.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(
+      apiKey: "AIzaSyD47UB7rDu3cFoC0GPx-fwsOiYiWEnh0gE",
+      authDomain: "practiceauth-4fc12.firebaseapp.com",
+      projectId: "practiceauth-4fc12",
+      storageBucket: "practiceauth-4fc12.firebasestorage.app",
+      messagingSenderId: "979223392484",
+      appId: "1:979223392484:web:0fb7c8022deda6af8e3cf1",
+      measurementId: "G-Z7MF0NV1WT"
+    ));
+  }else{
+    await Firebase.initializeApp();
+  }
+
+  if(kIsWeb){
+    runApp(
+      // Enable DevicePreview in Debug Builds Only
+      DevicePreview(
+        enabled: !kReleaseMode, // Disabled in release builds
+        builder: (context) => MyApp(),
+      ),
+    );
+  }else{
+    runApp(MyApp());
+  }
+}
+
+
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -1,43 +1,197 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_user_auth/screen/home.dart';
 
-class Signup extends StatelessWidget {
+class Signup extends StatefulWidget {
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  String email = "", password = "", name = "";
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+
+  bool _obscurePassword = true;
+  bool rememberPassword = true;
+  bool agreePersonalData = true;
+  final _formSignInKey = GlobalKey<FormState>();
+
+  registration() async {
+    if (password != null && namecontroller.text != "" && emailcontroller.text != "") {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Registered Successfully", style: TextStyle(fontSize: 20.0)),
+        ));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.orangeAccent,
+            content: Text("Password Provided is too Weak", style: TextStyle(fontSize: 18.0)),
+          ));
+        } else if (e.code == "email-already-in-use") {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.orangeAccent,
+            content: Text("Account Already exists", style: TextStyle(fontSize: 18.0)),
+          ));
+        }
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Sign Up', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              TextField(decoration: InputDecoration(labelText: 'Full Name')),
-              TextField(decoration: InputDecoration(labelText: 'Email')),
-              TextField(decoration: InputDecoration(labelText: 'Password'), obscureText: true),
-              SizedBox(height: 16),
-              ElevatedButton(onPressed: () {}, child: Text('Sign Up')),
-              SizedBox(height: 16),
-              Text('Sign in with'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      // Google login process
-                    },
-                    child: Image.asset(
-                      'assets/logo_google_g_icon.png',
-                      width: 32,
-                      height: 32,
+          child: Form(
+            key: _formSignInKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Sign Up', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter Name';
+                    }
+                    return null;
+                  },
+                  controller: namecontroller, //Add this
+                  decoration: InputDecoration(
+                    label: const Text('Full Name'),
+                    hintText: 'Enter Full Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                ],
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text("Already have an account? Login"),
-              )
-            ],
+                ),
+            
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter Email';
+                    }
+                    return null;
+                  },
+                  controller: emailcontroller, //Add this
+                  decoration: InputDecoration(
+                    label: const Text('Email'),
+                    hintText: 'Enter Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+            
+                TextFormField(
+                  obscureText: _obscurePassword,
+                  obscuringCharacter: '*',
+                  validator: (value){
+                    if(value == null || value.isEmpty)
+                    {
+                      return 'Please enter Password';
+                    }
+                    return null;
+                  },
+            
+                  controller: passwordcontroller,
+            
+                  decoration: InputDecoration(
+                    label: const Text('Password'),
+                    hintText: 'Enter Password',
+                    hintStyle: const TextStyle(
+                      color: Colors.black26,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.black12,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.black12,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+            
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                      ),
+            
+                      onPressed: (){
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      }
+                      ), 
+                    ),
+                  ),
+            
+                SizedBox(height: 16),
+            
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formSignInKey.currentState!.validate() && rememberPassword) {
+                      setState(() {
+                        email = emailcontroller.text;
+                        name = namecontroller.text;
+                        password = passwordcontroller.text;
+                      });
+                      registration();
+                    } else if (!rememberPassword) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Please agree to the processing of personal data')),
+                      );
+                    }
+                  },
+             
+                  child: Text('Sign Up')
+                ),
+            
+                SizedBox(height: 16),
+                Text('Sign in with'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Google login process
+                      },
+                      child: Image.asset(
+                        'assets/logo_google_g_icon.png',
+                        width: 32,
+                        height: 32,
+                      ),
+                    ),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Already have an account? Login"),
+                )
+              ],
+            ),
           ),
         ),
       ),
